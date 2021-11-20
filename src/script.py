296f210ad190d84f2d4a2cab324cb0742460ad24
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-11-20 07:54:08 +0000

    デコレータの説明終了

--- src/script.py
+++ src/script.py
@@ -15,8 +15,6 @@
 # f = print_info(add_num)
 # f(10, 20)
 #
-
-
 def print_info(func):
     def wrapper(*args, **keyargs):
         print("start")
@@ -27,7 +25,20 @@ def print_info(func):
     return wrapper
 
 
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print("func: ", func.__name__)
+        print("args: ", args)
+        print("kwargs: ", kwargs)
+        result = func(*args, **kwargs)
+        print("result: ", result)
+        return result
+
+    return wrapper
+
+
 @print_info
+@print_more
 def add_num(a, b):
     return a + b
 
