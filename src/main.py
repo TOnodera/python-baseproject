fe0865c1b70acd8f2e2fd9b41cbb5cdd27c8d3a9
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-11-21 09:17:45 +0000

    とりあえず動くようにした

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import csv
-from os import path, read, write
+from os import path
 import pathlib
 from termcolor import cprint
 
@@ -13,6 +13,21 @@ fieldnames = ["Name", "Count"]
 most_popular_restlan = None
 # 一番人気のあるお店のカウント
 most_popular_restlan_count = 0
+# CSVデータを保持する
+csv_file_list = []
+# 選択回答の入力(Yes)
+answer_yes = ["yes", "y"]
+# 選択回答の入力(No)
+answer_no = ["no", "n"]
+
+
+def get_recomend_restlan(restlan_list: list):
+    sorted_list = sorted(restlan_list, key=lambda x: x["Count"])
+    if len(sorted_list) > 0:
+        recomend_restlan_dict = sorted_list[0]
+        return recomend_restlan_dict["Name"]
+    return None
+
 
 # 名前を取得
 cprint("こんにちは！私はRobokoです。あなたの名前はなんですか？", "green")
@@ -22,17 +37,9 @@ while True:
         break
     cprint("名前を入力してください。", "red")
 
-# レストラン名を取得
-cprint(f"{name}さん。どこのレストランが好きですか？", "green")
-while True:
-    restlan_name = input()
-    if restlan_name:
-        break
-    cprint("レストラン名を入力してください。", "red")
-
 
 # ファイルが存在しない場合はファイルを作成する
-if not pathlib.Path.exists(csv_file_path):
+if not pathlib.Path(csv_file_path).exists():
     with open(csv_file_path, "w") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -40,6 +47,45 @@ if not pathlib.Path.exists(csv_file_path):
 # CSVファイルが存在する場合はファイルを読み込んでデータを取得する
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file, fieldnames=fieldnames)
+    for index, row in enumerate(reader):
+        if index == 0:
+            continue
+        csv_file_list.append(row)
+
+
+# おすすめがあれば表示
+recomend_restlan_name = get_recomend_restlan(csv_file_list)
+if recomend_restlan_name:
+    print(f"おすすめのレストラン{recomend_restlan_name}があります。お好きですか？")
+    while True:
+        answer = input()
+        if answer.lower() in answer_yes or answer.lower() in answer_no:
+            break
+        else:
+            cprint("Yes/Noで入力してください。", "red")
+
+
+# レストラン名を取得
+restlan_name = None
+cprint(f"{name}さん。どこのレストランが好きですか？", "green")
+while True:
+    restlan_name = input().capitalize()
+    if restlan_name:
+        break
+    cprint("レストラン名を入力してください。", "red")
+
+# CSVファイルを更新
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    # レストラン名がリストに既に存在する場合は+1
+    for index, row in enumerate(csv_file_list):
+        if restlan_name == row["Name"]:
+            row.update({"Count": int(row["Count"]) + 1})
+        writer.writerow(row)
+    # レストラン名がリストに存在しない場合は新規作成
+    if not any(restlan_name == row["Name"] for row in csv_file_list):
+        writer.writerow({"Name": restlan_name.capitalize(), "Count": 1})
 
 
 cprint(f"{name}さん。ありがとうございました。")
