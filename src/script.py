303266cdadaaa79f6c39191a5a7e5823060c4855
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-11-28 01:55:52 +0000

    スレッドとプロセス比較

--- src/script.py
+++ src/script.py
@@ -1,23 +1,74 @@
-import multiprocessing
+import concurrent.futures
+import os
 import time
 
-
-def measurement(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        func(*args, **kwargs)
-        print("実行時間: ", time.time() - start)
-
-    return wrapper
+LEARGET_TEXT = "some string" * 100000000
 
 
-def loop(n):
-    for i in range(n):
-        for j in range(n):
-            pass
+def io_bound(file_name):
+    with open(file_name, "w+") as f:
+        f.write(LEARGET_TEXT)
+        f.seek(0)
+        f.read()
+
+    os.remove(file_name)
+    return "Future is done!"
+
+
+def cpu_bound():
+    i = 0
+    while i < 1000000:
+        i = i + 1 - 2 + 3 - 4 + 5
+        return "Future is done!"
 
 
 if __name__ == "__main__":
-    proccess_num = 2
-    with multiprocessing.Pool(proccess_num) as p:
-        p.map(loop, [4, 4])
+    """
+    start = time.time()
+    print(io_bound("1.txt"))
+    print(io_bound("2.txt"))
+    end = time.time()
+    print("I/O bound: Sync {:.4f}".format(end - start))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        start = time.time()
+        future1 = executor.submit(io_bound, "1.txt")
+        future2 = executor.submit(io_bound, "2.txt")
+        print(future1.result())
+        print(future2.result())
+        end = time.time()
+        print("I/O bound: Thread {:.4f}".format(end - start))
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+        start = time.time()
+        future1 = executor.submit(io_bound, "1.txt")
+        future2 = executor.submit(io_bound, "2.txt")
+        print(future1.result())
+        print(future2.result())
+        end = time.time()
+        print("The number of cpu:{}".format(os.cpu_count()))
+        print("I/O bound: Process {:.4f}".format(end - start))
+    """
+    start = time.time()
+    print(cpu_bound())
+    print(cpu_bound())
+    end = time.time()
+    print("CPU bound: Sync {:.4f}".format(end - start))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        start = time.time()
+        future1 = executor.submit(cpu_bound)
+        future2 = executor.submit(cpu_bound)
+        print(future1.result())
+        print(future2.result())
+        end = time.time()
+        print("CPU bound: Thread {:.4f}".format(end - start))
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+        start = time.time()
+        future1 = executor.submit(cpu_bound)
+        future2 = executor.submit(cpu_bound)
+        print(future1.result())
+        print(future2.result())
+        end = time.time()
+        print("The number of cpu:{}".format(os.cpu_count()))
+        print("CPU bound: Process {:.4f}".format(end - start))
